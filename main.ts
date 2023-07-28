92c352f6721866d0d4a6d06a21b89cb394cc5e6c
flamrdevs <flamrdevs@gmail.com>
2023-07-28 18:23:57 +0700

    chore: use Deno.serve

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-import { serve } from "std/http/server.ts";
-
 import app from "~/app.ts";
 
 try {
-  await serve(app.fetch, {
-    port: Number(Deno.env.get("PORT") || 7000),
-    hostname: "0.0.0.0",
-    onListen: ({ hostname, port }) => {
-      console.log(`[flamrdevs-api]: ${hostname}:${port}`);
+  Deno.serve(
+    {
+      port: Number(Deno.env.get("PORT") || 7000),
+      hostname: "0.0.0.0",
+      onListen: ({ hostname, port }) => {
+        console.log(`[flamrdevs-api]: ${hostname}:${port}`);
+      },
     },
-  });
+    app.fetch
+  );
 } catch (error) {
   console.error(error);
 }
