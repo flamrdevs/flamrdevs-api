330130431b79cb0b870e218a08f6a9d848f4c462
flamrdevs <flamrdevs@gmail.com>
2023-08-20 18:23:09 +0700

    refactor: entry

--- main.ts
+++ main.ts
@@ -1,16 +1,13 @@
-import app from "~/app.ts";
+console.time("initialize");
+const app = (await import("~/app.ts")).default;
+console.timeEnd("initialize");
 
 try {
-  Deno.serve(
-    {
-      port: 7000,
-      hostname: "0.0.0.0",
-      onListen: ({ hostname, port }) => {
-        console.log(`[flamrdevs-api]: ${hostname}:${port}`);
-      },
-    },
-    app.fetch
-  );
+  const hostname = "0.0.0.0";
+  const port = 7000;
+  const onListen = () => console.log(`[flamrdevs-api]: ${hostname}:${port}`);
+  const server = Deno.serve({ port, hostname, onListen }, app.fetch);
+  await server.finished;
 } catch (error) {
   console.error(error);
 }
