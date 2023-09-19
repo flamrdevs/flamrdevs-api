62eb2062ce2e6787fb86cef78b22d84ca05a1e4b
flamrdevs <flamrdevs@gmail.com>
2023-09-19 17:31:43 +0700

    chore: update github route

--- src/routes/github.ts
+++ src/routes/github.ts
@@ -1,15 +1,11 @@
 import { route } from "~/libs/hono.ts";
-import { UsernameSchema, ReponameSchema, UserSchema, OrgSchema, RepoSchema, getUser, getOrg, getRepo } from "~/libs/github.ts";
+import { UsernameSchema, ReponameSchema, UserSchema, RepoSchema, getUser, getRepo } from "~/libs/github.ts";
 
 export default route((x) =>
   x
 
     .get("/users/:username", async (ctx) => {
       return ctx.json(await UserSchema.parseAsync(await getUser(await UsernameSchema.parseAsync(ctx.req.param("username")))));
-    })
-
-    .get("/orgs/:org", async (ctx) => {
-      return ctx.json(await OrgSchema.parseAsync(await getOrg(await UsernameSchema.parseAsync(ctx.req.param("org")))));
     })
 
     .get("/repos/:owner/:repo", async (ctx) => {
